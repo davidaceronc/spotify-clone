edf1752c82280da5beb41c591355363004754e42
David Ceron <davidaceronc@gmail.com>
2021-07-12 03:39:32 -0500

    Autoplay implemented in Spotify Player

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -1,11 +1,19 @@
 import SpotifyPlayer from 'react-spotify-web-playback'
+import { useEffect, useState } from "react";
 
 export default function Player({accessToken, songUrl}: {accessToken: string, songUrl: string}) {
+    const [play, setPlay] = useState(false)
+
+    useEffect(() => setPlay(true), [songUrl])
     if ( accessToken ) {
         return <SpotifyPlayer
             token={accessToken}
             showSaveIcon
             uris={songUrl ? [songUrl] : []}
+            callback={ state =>{
+                if (!state.isPlaying) setPlay(false)
+            }}
+            play={play}
         />
     } else {
         return <div>No Access Token Provided</div>
